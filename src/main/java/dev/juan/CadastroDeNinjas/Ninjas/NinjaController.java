23c5d0b1f8bdc96e4130d950864c030c85f662d4
Juan Peruzzo <juanperuzzo13@gmail.com>
2025-09-21 08:53:19 -0300

    fix: adiciona a rota /ninjas na annotation @RequestMapping

--- src/main/java/dev/juan/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/juan/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -3,7 +3,7 @@ package dev.juan.CadastroDeNinjas.Ninjas;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping("/ninjas")
 public class NinjaController {
 
     @GetMapping("/boasvindas")
