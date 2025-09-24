10e71584afef2eb9a0d2f92ddc3fa8fedaf48b4b
Juan Peruzzo <juanperuzzo13@gmail.com>
2025-09-23 22:55:24 -0300

    feat: Inclui o método PUT, porém ainda é preciso trabalhar em alterações parciais

--- src/main/java/dev/juan/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/juan/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -37,9 +37,9 @@ public class NinjaController {
         return ninjaService.listarNinjasPorId(id);
     }
     // Alterar dados dos ninjas (UPDATE)
-    @PutMapping("/alterarID")
-    public String alterarNinjaPorId() {
-        return "Alterar ninja por id";
+    @PutMapping("/alterar/{id}")
+    public NinjaModel alterarNinjaPorId(@PathVariable Long id, @RequestBody NinjaModel ninjaAtualizado) {
+        return ninjaService.atualizarNinja(id, ninjaAtualizado);
     }
 
     // Deletar ninja (DELETE)
