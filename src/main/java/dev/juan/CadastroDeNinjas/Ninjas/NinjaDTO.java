ef7a8eb1be3e8533070b475b25d3d3eaed29f4c2
Juan Peruzzo <juanperuzzo13@gmail.com>
2025-09-25 08:38:31 -0300

    chore: Inclui a versão do DTO pela entidade e mapeia manualmente no NinjaMapper os caminhos entidade x DTO

--- src/main/java/dev/juan/CadastroDeNinjas/Ninjas/NinjaDTO.java
+++ src/main/java/dev/juan/CadastroDeNinjas/Ninjas/NinjaDTO.java
@@ -1,4 +1,20 @@
 package dev.juan.CadastroDeNinjas.Ninjas;
 
+import dev.juan.CadastroDeNinjas.Missoes.MissoesModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NinjaDTO {
+
+    private Long id;
+    private String nome;
+    private String email;
+    private String imgUrl;
+    private int idade;
+    private String rank;
+    private MissoesModel missoes;
 }
